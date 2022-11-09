a2ea15710718a19f5fe2ff02d2b13ff82c611f9d
michelleacg <111101012+michelleacg@users.noreply.github.com>
2022-11-09 14:47:17 -0800

    7.3.1

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -17,7 +17,7 @@ CREATE TABLE employees (
 );
 
 CREATE TABLE dept_manager (
-dept_no VARCHAR(4) NOT NULL,
+    dept_no VARCHAR NOT NULL,
     emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
@@ -35,19 +35,20 @@ FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
     PRIMARY KEY (emp_no)
 );
 
-CREATE TABLE dept_emp
-    emp_no INT NOT NULL,
-    dept_name VARCHAR(40) NOT NULL,
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+    dept_no VARCHAR  NOT NULL,
     from_date DATE NOT NULL,
-    to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    PRIMARY KEY (emp_no)
+    to_date DATE NOT NULL
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
 
-CREATE TABLE titles
+CREATE TABLE titles (
     emp_no INT NOT NULL,
     title VARCHAR NOT NULL, 
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
-FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    PRIMARY KEY (emp_no)
+    PRIMARY KEY (emp_no, title, from_date)
+);
