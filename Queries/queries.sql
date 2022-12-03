a34a4d5d64fa289021fc3e2d224a702a537dcf28
michelleacg <111101012+michelleacg@users.noreply.github.com>
2022-12-02 18:08:55 -0800

    commit

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -19,10 +19,60 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- drop table retirement_info;
-SELECT first_name, last_name
+SELECT emp_no, first_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
+-- Check the table
 SELECT * from retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT departments.dept_name,
+     dept_manager.emp_no,
+     dept_manager.from_date,
+     dept_manager.to_date
+FROM departments
+INNER JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no;
+
+-- Joining retirement_info and dept_emp tables
+SELECT retirement_info.emp_no,
+    retirement_info.first_name,
+retirement_info.last_name,
+    dept_emp.to_date
+FROM retirement_info
+LEFT JOIN dept_emp
+ON retirement_info.emp_no = dept_emp.emp_no;
+
+--Use Aliases for Code Readability with join retirement_info and dept_emp tables. Rename retirement_info as "ri" and dept_emp as "de"
+
+SELECT ri.emp_no,
+    ri.first_name,
+ri.last_name,
+    de.to_date
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no;
+
+-- Use Aliases for Code Readability with join  departments and dept_manager tables. Rename departments to "d" and dept_manager to "dm."
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments as d
+INNER JOIN dept_manager as dm
+ON d.dept_no = dm.dept_no;
+
+--Use Aliases for Code Readability with join retirement_info and dept_emp tables. Rename retirement_info as "ri" and dept_emp as "de". Use Left Join for retirement_info and dept_emp tables
+
+SELECT ri.emp_no,
+ri.first_name,
+ri.last_name,
+de.to_date
+INTO current_emp
+
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
